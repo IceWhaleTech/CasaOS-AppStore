f23145edc959a5ec2414d5c9cefe3efc8365f963
Tiger Wang <tigerwang@outlook.com>
2023-03-01 15:31:40 -0500

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- build/scripts/setup/script.d/99-setup-appstore.sh
+++ build/scripts/setup/script.d/99-setup-appstore.sh
@@ -2,16 +2,29 @@
 
 set -e 
 
-BUILD_PATH=$(dirname "${BASH_SOURCE[0]}")/../../..
-readonly BUILD_PATH
+DEFAULT_APPSTORE_PATH="/var/lib/casaos/appstore/default"
 
-DEFAULT_APPSTORE_PATH="${BUILD_PATH}/sysroot/var/lib/casaos/appstore/default"
+if [ -d "${DEFAULT_APPSTORE_PATH}" ]; then
+    echo "🟩 Backup existing default appstore..."
+    mv -f "${DEFAULT_APPSTORE_PATH}" "${DEFAULT_APPSTORE_PATH}.old" || {
+        echo "🟥 Failed to backup existing default appstore"
+        exit 1
+    }
+fi
 
-mv -f "${DEFAULT_APPSTORE_PATH}" "${DEFAULT_APPSTORE_PATH}.old"
-mv -f "${DEFAULT_APPSTORE_PATH}.new" "${DEFAULT_APPSTORE_PATH}" || {
-    rm -f "${DEFAULT_APPSTORE_PATH}"
-    mv -f "${DEFAULT_APPSTORE_PATH}.old" "${DEFAULT_APPSTORE_PATH}"
-    rm -rf "${DEFAULT_APPSTORE_PATH}.new"
-    exit 1
-}
-rm -rf "${DEFAULT_APPSTORE_PATH}.old"
+echo "🟩 Updating default appstore..."
+
+if [ -d "${DEFAULT_APPSTORE_PATH}.new" ]; then
+    mv -vf "${DEFAULT_APPSTORE_PATH}.new" "${DEFAULT_APPSTORE_PATH}" || {
+        echo "🟥 Failed to update default appstore... restoring backup..."
+        rm -vf "${DEFAULT_APPSTORE_PATH}"
+        mv -vf "${DEFAULT_APPSTORE_PATH}.old" "${DEFAULT_APPSTORE_PATH}"
+        rm -rvf "${DEFAULT_APPSTORE_PATH}.new"
+        exit 1
+    }
+    rm -rvf "${DEFAULT_APPSTORE_PATH}.old" || {
+        echo "🟨 Failed to remove old default appstore backup"
+    }
+else
+    echo "🟨 New default appstore does not exist"
+fi
